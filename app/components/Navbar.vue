e3072295cabb259d63d3aded94144ad23889b215
OG-TenLetters <1tenletts0@gmail.com>
2026-04-20 12:33:46 -0700

    I've cleaned up the Searchbar aesthetic, and Connected the AnithonLogo to be a default redirect

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -8,20 +8,68 @@ import OptionsModal from "./Navbar/OptionsModal.vue";
   <div
     class="m-auto flex w-full max-w-480 items-center justify-between gap-x-2 rounded-b-lg bg-sky-800 px-6 py-5 opacity-90 inset-shadow-sm inset-shadow-blue-400 backdrop-blur-3xl"
   >
-    <AnithonLogo class="ml-15" />
-    <div
-      class="flex w-full max-w-140 items-center gap-x-2 rounded-md bg-white px-4 py-2"
-    >
-      <Icon class="text-lg" name="mingcute:search-3-line" />
-      <input name="search" id="search" placeholder="Search Anime..." />
+    <NuxtLink to="/">
+      <AnithonLogo
+        class="ml-15 cursor-pointer transition-all duration-150 hover:brightness-90"
+      />
+    </NuxtLink>
+    <div class="relative">
+      <input
+        class="outline-none focus:ring-3 ring-sky-950/50  inset-shadow-sm inset-shadow-neutral-500 flex w-140 items-center gap-x-2 rounded-md bg-white px-4 py-2 pl-8"
+        name="search"
+        id="search"
+        placeholder="Search..."
+      />
+      <Icon
+        class="absolute text-blue-950 top-1/2 left-2 -translate-y-4/9 text-xl"
+        name="mingcute:search-3-line"
+      />
+      <button class="hover:brightness-170 p-4 cursor-pointer text-blue-950 absolute right-2 top-1/2 -translate-y-1/2 flex items-center font-semibold uppercase">
+        <Icon name="mingcute:filter-fill" />
+        Filter
+      </button>
     </div>
     <div class="mx-10 flex gap-x-7 font-semibold text-white uppercase">
-      <OptionsModal name="Genres" link-frag-items="/genres" link-frag2-items="" url="mock-url"/>
-      <OptionsModal name="Type" link-frag-items="" link-frag2-items="" url="mock-url"/>
-      <NavLink class="hover:text-neutral-200" name="New Releases" linkFrag="/new-releases" link-frag2="" url="mock-url" />
-      <NavLink class="hover:text-neutral-200" name="Updates" linkFrag="/updates" link-frag2="" url="mock-url" />
-      <NavLink class="hover:text-neutral-200" name="Ongoing" linkFrag="/ongoing" link-frag2="" url="mock-url" />
-      <NavLink class="hover:text-neutral-200" name="Recent" linkFrag="/recent" link-frag2="" url="mock-url" />
+      <OptionsModal
+        name="Genres"
+        link-frag-items="/genres"
+        link-frag2-items=""
+        url="mock-url"
+      />
+      <OptionsModal
+        name="Type"
+        link-frag-items=""
+        link-frag2-items=""
+        url="mock-url"
+      />
+      <NavLink
+        class="hover:text-neutral-200"
+        name="New Releases"
+        linkFrag="/new-releases"
+        link-frag2=""
+        url="mock-url"
+      />
+      <NavLink
+        class="hover:text-neutral-200"
+        name="Updates"
+        linkFrag="/updates"
+        link-frag2=""
+        url="mock-url"
+      />
+      <NavLink
+        class="hover:text-neutral-200"
+        name="Ongoing"
+        linkFrag="/ongoing"
+        link-frag2=""
+        url="mock-url"
+      />
+      <NavLink
+        class="hover:text-neutral-200"
+        name="Recent"
+        linkFrag="/recent"
+        link-frag2=""
+        url="mock-url"
+      />
     </div>
     <div class="flex items-center gap-x-4">
       <LanguageToggle />
@@ -32,7 +80,7 @@ import OptionsModal from "./Navbar/OptionsModal.vue";
         <img
           class="absolute top-auto left-auto scale-140 -rotate-45 bg-amber-300"
           src="/Anime-Profile-Example.webp"
-          alt=""
+          alt="Profile Image"
         />
       </figure>
     </div>
