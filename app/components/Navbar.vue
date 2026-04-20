8906808e22baa7c048b4e0e6672e8d959d955903
OG-TenLetters <1tenletts0@gmail.com>
2026-04-20 13:17:24 -0700

    I'm learning to separate chunks that are layered. So I'm separating the profile modals even though they will live on the Navbar

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -15,16 +15,18 @@ import OptionsModal from "./Navbar/OptionsModal.vue";
     </NuxtLink>
     <div class="relative">
       <input
-        class="outline-none focus:ring-3 ring-sky-950/50  inset-shadow-sm inset-shadow-neutral-500 flex w-140 items-center gap-x-2 rounded-md bg-white px-4 py-2 pl-8"
+        class="flex w-140 items-center gap-x-2 rounded-md bg-white px-4 py-2 pl-8 inset-shadow-sm ring-sky-950/50 inset-shadow-neutral-500 outline-none focus:ring-3"
         name="search"
         id="search"
         placeholder="Search..."
       />
       <Icon
-        class="absolute text-blue-950 top-1/2 left-2 -translate-y-4/9 text-xl"
+        class="absolute top-1/2 left-2 -translate-y-4/9 text-xl text-blue-950"
         name="mingcute:search-3-line"
       />
-      <button class="hover:brightness-170 p-4 cursor-pointer text-blue-950 absolute right-2 top-1/2 -translate-y-1/2 flex items-center font-semibold uppercase">
+      <button
+        class="absolute top-1/2 right-2 flex -translate-y-1/2 cursor-pointer items-center p-4 font-semibold text-blue-950 uppercase hover:brightness-170"
+      >
         <Icon name="mingcute:filter-fill" />
         Filter
       </button>
@@ -74,15 +76,24 @@ import OptionsModal from "./Navbar/OptionsModal.vue";
     <div class="flex items-center gap-x-4">
       <LanguageToggle />
       <Icon class="text-2xl text-white" name="mingcute:sun-fill" />
-      <figure
+      <div class="relative">
+        <figure
         class="h-9 w-9 rotate-45 overflow-hidden rounded-xs bg-white shadow-lg shadow-sky-900"
-      >
+        >
         <img
-          class="absolute top-auto left-auto scale-140 -rotate-45 bg-amber-300"
-          src="/Anime-Profile-Example.webp"
-          alt="Profile Image"
+        class="absolute top-auto left-auto scale-140 -rotate-45 bg-amber-300"
+        src="/Anime-Profile-Example.webp"
+        alt="Profile Image"
         />
+        
       </figure>
+        <ul class="bg-black absolute right-0 flex flex-col gap-y-3 py-4 px-6 rounded-xl text-white text-sm">
+          <li>Random</li>
+          <li>Random</li>
+          <li>Random</li>
+          <li>Random</li>
+        </ul>
+    </div>
     </div>
   </div>
 </template>
