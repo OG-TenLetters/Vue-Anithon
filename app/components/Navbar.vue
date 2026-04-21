f89eacce0b52796cc829aab08835d601d36550aa
OG-TenLetters <1tenletts0@gmail.com>
2026-04-21 13:29:30 -0700

    First half of the Profile Modal stylized and done

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -2,11 +2,33 @@
 import LanguageToggle from "./Navbar/LanguageToggle.vue";
 import NavLink from "./Navbar/NavLink.vue";
 import OptionsModal from "./Navbar/OptionsModal.vue";
+import ThemeToggle from "./Navbar/ThemeToggle.vue";
+const isProfileModalOpen = ref(false);
+const isSignedIn = ref(true); // CHANGE LATER
+const isSigninModalOpen = ref(false);
+
+const handleUserAuth = () => {
+  if (isSignedIn.value === false) {
+    isProfileModalOpen.value = false;
+    isSigninModalOpen.value = true;
+  } else {
+    isProfileModalOpen.value = !isProfileModalOpen.value;
+  }
+
+  console.log("SignedIn?:", isSignedIn.value);
+  console.log("Open Signin?:", isSigninModalOpen.value);
+  console.log("Open Profile Menu?:", isProfileModalOpen.value);
+};
+
+const toggleProfileModal = () => {
+  isProfileModalOpen.value = !isProfileModalOpen.value;
+  console.log(isProfileModalOpen.value);
+};
 </script>
 
 <template>
   <div
-    class="m-auto flex w-full max-w-480 items-center justify-between gap-x-2 rounded-b-lg bg-sky-800 px-6 py-5 opacity-90 inset-shadow-sm inset-shadow-blue-400 backdrop-blur-3xl"
+    class="m-auto flex w-full max-w-480 items-center justify-between gap-x-2 rounded-b-lg bg-sky-800 px-6 py-5 opacity-90 inset-shadow-[0px_-1px_4px_2px] inset-shadow-sky-600/90 backdrop-blur-3xl"
   >
     <NuxtLink to="/">
       <AnithonLogo
@@ -75,25 +97,56 @@ import OptionsModal from "./Navbar/OptionsModal.vue";
     </div>
     <div class="flex items-center gap-x-4">
       <LanguageToggle />
-      <Icon class="text-2xl text-white" name="mingcute:sun-fill" />
-      <div class="relative">
+      <ThemeToggle />
+      <div @click="handleUserAuth" class="relative">
         <figure
-        class="h-9 w-9 rotate-45 overflow-hidden rounded-xs bg-white shadow-lg shadow-sky-900"
+          :class="isProfileModalOpen ? 'rotate-0' : 'rotate-45'"
+          class="h-9 w-9 cursor-pointer overflow-hidden rounded-xs bg-white shadow-lg shadow-sky-900 transition-all duration-300 hover:brightness-90"
         >
-        <img
-        class="absolute top-auto left-auto scale-140 -rotate-45 bg-amber-300"
-        src="/Anime-Profile-Example.webp"
-        alt="Profile Image"
-        />
-        
-      </figure>
-        <ul class="bg-black absolute right-0 flex flex-col gap-y-3 py-4 px-6 rounded-xl text-white text-sm">
-          <li>Random</li>
-          <li>Random</li>
-          <li>Random</li>
-          <li>Random</li>
+          <img
+            :class="isProfileModalOpen ? 'rotate-0' : '-rotate-45'"
+            class="absolute top-auto left-auto scale-140 -rotate-45 bg-amber-300 transition-all duration-300"
+            src="/Anime-Profile-Example.webp"
+            alt="Profile Image"
+          />
+        </figure>
+        <ul
+          :class="
+            isProfileModalOpen
+              ? 'translate-y-0.5 scale-y-100 opacity-100'
+              : '-translate-y-1/2 scale-y-0 opacity-0'
+          "
+          class="absolute right-0 flex flex-col gap-y-2 rounded-3xl rounded-tr-none bg-gray-800/80 px-4 py-6 pb-3 text-sm text-white inset-shadow-[0px_-4px_4px_2px] inset-shadow-sky-500/40 transition-all duration-300"
+        >
+          <div class="rounded-xl rounded-tr-none border-2 border-sky-500/40">
+            <li
+              class="cursor-pointer px-6 py-3 transition-all duration-150 first:rounded-tl-xl hover:bg-gray-700 hover:text-neutral-300"
+            >
+              Dashboard
+            </li>
+            <li
+              class="cursor-pointer px-6 py-2 transition-all duration-150 hover:bg-gray-700 hover:text-neutral-300"
+            >
+              Edit Profile
+            </li>
+            <li
+              class="cursor-pointer px-6 py-2 transition-all duration-150 hover:bg-gray-700 hover:text-neutral-300"
+            >
+              Notifications
+            </li>
+            <li
+              class="cursor-pointer px-6 py-3 transition-all duration-150 last:rounded-b-xl hover:bg-gray-700 hover:text-neutral-300"
+            >
+              Settings
+            </li>
+          </div>
+          <li
+            class="cursor-pointer rounded-b-xl border-t-2 border-gray-500/40 px-6 py-2 text-center transition-all duration-150 hover:bg-gray-700 hover:text-neutral-300"
+          >
+            Logout
+          </li>
         </ul>
-    </div>
+      </div>
     </div>
   </div>
 </template>
---
+++ app/components/Navbar/ThemeToggle.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const isThemeToggled = ref(false);
+
+const toggleTheme = () => {
+  isThemeToggled.value = !isThemeToggled.value;
+};
+</script>
+
+<template>
+  <Icon
+    @click="toggleTheme"
+    class="cursor-pointer w-6 h-6 text-2xl text-white"
+    :name="isThemeToggled ? 'mingcute:moon-stars-fill' : 'mingcute:sun-fill'"
+  />
+</template>
