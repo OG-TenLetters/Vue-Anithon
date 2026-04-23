f6e1282f692429f7164b2b4d6a4dc2f78061e47c
OG-TenLetters <1tenletts0@gmail.com>
2026-04-22 17:48:47 -0700

    Idea colors for later

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,17 +8,17 @@ import AnimeCarousel from '~/components/Pages/HomePage/AnimeCarousel.vue';
     <AnimeCarousel />
     <div class="flex flex-col gap-y-4 px-10">
       <div
-        class="flex h-80 w-full items-center justify-center rounded-3xl bg-black text-white"
+        class="flex h-80 w-full items-center justify-center rounded-3xl bg-[#1c0d35]/90 text-white"
       >
         <h1>Yes</h1>
       </div>
       <div
-        class="flex h-80 w-full items-center justify-center rounded-3xl bg-black text-white"
+        class="flex h-80 w-full items-center justify-center rounded-3xl bg-[#031a25]/90 text-white"
       >
         <h1>Yes</h1>
       </div>
       <div
-        class="flex h-80 w-full items-center justify-center rounded-3xl bg-black text-white"
+        class="flex h-80 w-full items-center justify-center rounded-3xl bg-[#0f003a]/90 text-white"
       >
         <h1>Yes</h1>
       </div>
